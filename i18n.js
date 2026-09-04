425ace4cddc570d96bd4b9ba386717c3e857b04f
vladi <zubkovla@fel.cvut.cz>
2026-09-04 10:13:43 +0200

    Clip collapsed photo 9px at viewport top.

--- i18n.js
+++ i18n.js
@@ -575,7 +575,7 @@
     var PHOTO_FULL = 160;
     var PHOTO_MIN = 110;
     var BRAND_MIN = 28;
-    var PHOTO_CLIP_ABOVE = 5;
+    var PHOTO_CLIP_ABOVE = 9;
     var COLLAPSE_RANGE = 180;
     var metrics = null;
     var raf = 0;
